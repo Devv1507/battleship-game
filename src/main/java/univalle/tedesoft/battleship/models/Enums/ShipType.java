8d93df4ccb2a59995c9bb26dc6c1d2f47ca523d0
Santiago Guerrero <santiago.david.guerrero@correounivalle.edu.co>
2025-05-27 11:25:50 -0500

    Enums necessaries

---
+++ src/main/java/univalle/tedesoft/battleship/models/Enums/ShipType.java
@@ -0,0 +1,18 @@
+package univalle.tedesoft.battleship.models.Enums;
+
+/**
+ * Enum que respresenta los tipos de barcos que se usan en el juego.
+ * @author David Esteban Valencia
+ * @author Santiago David Guerrero
+ * @author Juan Pablo Escamilla
+ */
+public enum ShipType {
+    /** Embarcacion de tipo Portaavionnes*/
+    AIR_CRAFT_CARRIER,
+    /** Embarcacion de tipo Submarino*/
+    SUBMARINE,
+    /** Embarcacion de tipo Destructor*/
+    DESTROYER,
+    /** Embarcacion de tipo Fragata*/
+    FRIGATES
+}
