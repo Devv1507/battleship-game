bcfac9f579ff05497520bec9ccff97b6499ec2c1
Santiago Guerrero <santiago.david.guerrero@correounivalle.edu.co>
2025-05-28 16:35:47 -0500

    Created player class and your sons

---
+++ src/main/java/univalle/tedesoft/battleship/models/Players/Player.java
@@ -0,0 +1,20 @@
+package univalle.tedesoft.battleship.models.Players;
+
+/**
+ * Clase que representa al jugador.
+ * @author David Esteban Valencia
+ * @author Santiago David Guerrero
+ * @author Juan Pablo Escamilla
+ */
+public abstract class Player {
+    /**Nombre del jugador*/
+    private final String name;
+    /**Constructor de la clase*/
+    public Player(String name) {
+        this.name = name;
+    }
+    /**Metodo para obtener el nombre del jugador*/
+    public String getName() {
+        return name;
+    }
+}
