7a7012f68edac411166fa6e7205a64534f0ba538
Devv1507 <davide.valencia.v@gmail.com>
2025-07-25 23:53:53 -0500

    docs: updated IShape JavaDocs

--- src/main/java/univalle/tedesoft/battleship/views/shapes/IShape.java
+++ src/main/java/univalle/tedesoft/battleship/views/shapes/IShape.java
@@ -3,22 +3,20 @@ package univalle.tedesoft.battleship.views.shapes;
 import javafx.scene.Node;
 
 /**
- * Interfaz que define el contrato para todas las representaciones visuales de los marcadores de disparo.
- * Cada clase que implemente esta interfaz será responsable de crear una forma específica
- * (agua, humo, llama) para representar el resultado de un disparo en el tablero.
+ * Interfaz que define el contrato para todas las representaciones visuales (barcos y marcadores)
+ * Cada clase que implemente esta interfaz será responsable de crear una forma específica,
+ * como un barco o un marcador, y devolver un nodo de JavaFX que lo represente.
  */
 public interface IShape {
 
     /**
-     * Tamaño de la celda en la que se dibujará el marcador.
-     * Es útil tenerlo como una constante compartida.
+     * Tamaño de la celda en la que se dibujará la forma.
      */
     double CELL_SIZE = 30.0;
 
     /**
-     * Crea y devuelve el nodo de JavaFX que representa visualmente el marcador.
-     *
-     * @return Un nodo de JavaFX (generalmente un Group) que representa el marcador.
+     * Crea y devuelve el nodo de JavaFX que representa visualmente el objeto que implementa esta interfaz.
+     * @return Un nodo de JavaFX (generalmente un Group) que representa el barco o el marcador.
      */
     Node createShape();
 }
